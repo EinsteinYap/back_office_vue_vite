146c5127954c4fefe74e02ea0952ab8fb2e9e79f
EinsteinYap <einsteinyap2014@gmail.com>
2024-01-02 16:56:04 +0800

    added :sm and :md for responsive web page

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
         <el-row class="bg-indigo-500  min-h-screen">
-            <el-col :span="16" class="flex items-center justify-center " >
+            <el-col :sm="12" :md="16" class="flex items-center justify-center " >
                 <div>
                     <div class="font-bold text-5xl text-light-50 mb-4">欢迎光临</div>
                     <div class="text-gray-200 text-sm">Vue 3 + vite 商城后台</div>
                 </div>
             </el-col>
-            <el-col :span="8" class="bg-light-50 flex items-center justify-center flex-col">
+            <el-col :sm="12" :md="8" class="bg-light-50 flex items-center justify-center flex-col">
                 <h2 class="font-bold text-3xl text-gray-800 ">欢迎回来</h2>
                 <div class="flex items-center justify-center my-5 space-x-2 text-gray-300">
                 <span class="h-[1px] w-16 bg-gray-200"></span>
