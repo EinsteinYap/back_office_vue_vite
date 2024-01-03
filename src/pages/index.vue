b08c6135acbd238c634e26a5031f83f09429424d
EinsteinYap <einsteinyap2014@gmail.com>
2024-01-03 14:00:08 +0800

    sample script setup at index.vue has been coded

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,33 @@
 <template>
     <div>
         后台首页
+
+        <el-button @click="addCount">
+            {{ count }}
+        </el-button>
+        <el-button @click="addCount2" type="primary">
+            {{ form.count }}
+        </el-button>
+
+        <!-- <HelloWorld /> -->
+        
     </div>
 </template>
+
+<script setup>
+// import HelloWorld from '~/components/HelloWorld.vue'
+import { ref,reactive } from 'vue'
+let count =ref(1) 
+
+function addCount(){
+    count.value ++
+}
+
+const form = reactive({
+ count :2
+})
+
+function addCount2(){
+    form.count ++
+}
+</script>
