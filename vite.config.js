d2b49b3b2b0089982351b5846754eaf107cc0300
EinsteinYap <einsteinyap2014@gmail.com>
2024-01-04 21:59:29 +0800

    added dynamic meta title, nprogress as global loading

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
 
-import path from 'path'
+import path from "path"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{   
-    port:3000, 
-    proxy:{
-      "/api": {
-        ws: true,
-        changeOrigin: true,
-        target: "http://ceshi13.dishait.cn",
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, "")
-      }
-    }
-  },
-  // devServer:{    
-  //   port: 3000,
-  //   proxy:{
-  //     "/api": {
-  //       ws: true,
-  //       changeOrigin: true,
-  //       target: "http://ceshi13.dishait.cn/",
-  //       secure: false,
-  //       rewrite: (path) => path.replace(/^\/api/, "")
-  //     }
-  //   }
-  // },
   resolve:{
     alias:{
       "~":path.resolve(__dirname,"src")
     }
   },
-  plugins: [vue(),WindiCSS()],
+
+  server:{
+    proxy:{
+      '/api': {
+        target: 'http://ceshi13.dishait.cn',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  },
+
+  plugins: [vue(),WindiCSS()]
 })
