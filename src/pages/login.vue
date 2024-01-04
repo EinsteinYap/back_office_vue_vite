b847841501d08e76d8b846277a1ab69c820e65e0
EinsteinYap <einsteinyap2014@gmail.com>
2024-01-04 18:56:58 +0800

    api axios setup and cookies setup ready

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,113 +1,133 @@
 <template>
-    <div>
-        <el-row class="login-container">
-            <el-col :sm="12" :md="16" class="left" >
-                <div>
-                    <div class="">欢迎光临</div>
-                    <div class="">Vue 3 + vite 商城后台</div>
-                </div>
-            </el-col>
-            <el-col :sm="12" :md="8" class="right">
-                <h2 class="title">欢迎回来</h2>
-                <div class="">
+    <el-row class="login-container">
+        <el-col :lg="16" :md="12" class="left">
+            <div>
+                <div>欢迎光临</div>
+                <div>此站点是《vue3 + vite实战商城后台开发》视频课程的演示地址</div>
+            </div>
+        </el-col>
+        <el-col :lg="8" :md="12" class="right">
+            <h2 class="title">欢迎回来</h2>
+            <div>
                 <span class="line"></span>
                 <span>账号密码登录</span>
                 <span class="line"></span>
-                </div>
-             
-                <el-form ref="formRef" :model="form" :rules="rules" class="w-[250px]">
-                    <el-form-item label="" prop="username">
-                      <el-input v-model="form.username" placeholder="请输入用户名" >
+            </div>
+            <el-form ref="formRef" :rules="rules" :model="form" class="w-[250px]">
+                <el-form-item prop="username">
+                    <el-input v-model="form.username" placeholder="请输入用户名">
                         <template #prefix>
-                         <el-icon>
-                          <user />
-                        </el-icon>
-                        </template>  
-                      </el-input>
-                    </el-form-item>
-
-                    <el-form-item label="" prop="password">
-                    <el-input type="password" show-password v-model="form.password"  placeholder="请输入密码" >
-                      <template #prefix>
-                         <el-icon>
-                          <lock />
-                        </el-icon>
-                        </template>   
+                            <el-icon><user /></el-icon>
+                        </template>
                     </el-input>
-                    </el-form-item>
-    <el-form-item >
-      <el-button class="w-[250px] " round color="#626aef" type="primary" @click="onSubmit">登录</el-button>
-    </el-form-item>
-  </el-form>
-            </el-col>
-            
-        </el-row>
-        
-    </div>
-    
+                </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" v-model="form.password" placeholder="请输入密码" show-password>
+                        <template #prefix>
+                            <el-icon><lock /></el-icon>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button round color="#626aef" class="w-[250px]" type="primary" @click="onSubmit">登 录</el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
 </template>
+
 <script setup>
-import { reactive, ref } from 'vue'
-// import { User ,Lock } from '@element-plus/icons-vue'
+import { ref,reactive } from 'vue'
+import { ElNotification } from 'element-plus'
+import { useRouter } from 'vue-router'
+import { login } from '~/api/manager'
+import { useCookies } from '@vueuse/integrations/useCookies'
+
+const router = useRouter()
 
 // do not use same name with ref
 const form = reactive({
-  username: '',
-  password:''
+  username:"",
+  password:""
 })
 
-const rules ={
-  username:[
-  { required: true, message: '用户名不能为空', trigger: 'blur' },
-  { min: 3, max: 5, message: '用户名的长度3到5个字符', trigger: 'blur' },
-  ],
-  password:[
-  { required: true, message: '密码不能为空', trigger: 'blur' },
-
-  ]
+const rules = {
+    username:[
+        { 
+            required: true, 
+            message: '用户名不能为空', 
+            trigger: 'blur' 
+        },
+    ],
+    password:[
+        { 
+            required: true, 
+            message: '用户名不能为空', 
+            trigger: 'blur' 
+        },
+    ]
 }
 
-const formRef =ref(null)
+const formRef = ref(null)
 
 const onSubmit = () => {
-  formRef.value.validate((valid)=>{
-    if(!valid){
-    return false
-  }
-  console.log('validation done')
-  }
+    formRef.value.validate((valid)=>{
+        if(!valid){
+            return false
+        }
+        login(form.username,form.password)
+        .then(res=>{
+            console.log(res.data.data);
+
+            // 提示成功
+            ElNotification({
+                message: "登录成功",
+                type: 'success',
+                duration:3000
+            })
+
+            // 存储token和用户相关信息，下节课讲
+            const cookie = useCookies()
+            cookie.set("admin-token",res.data.data.token)
 
 
-  )
-  console.log('form submit!')
+            // 跳转到后台首页
+            router.push("/")
+        })
+        .catch(err=>{
+            ElNotification({
+                message: err.response.data.msg || "请求失败",
+                type: 'error',
+                duration:3000
+            })
+        })
+    })
 }
 </script>
 
-<style scope>
+<style scoped>
 .login-container{
-  @apply bg-indigo-500  min-h-screen;
+    @apply min-h-screen bg-indigo-500;
 }
-.left{
-  @apply flex items-center justify-center;
+.login-container .left,.login-container .right{
+    @apply flex items-center justify-center;
 }
-.right{
-  @apply bg-light-50 flex items-center justify-center flex-col;
+.login-container .right{
+    @apply bg-light-50 flex-col;
 }
 .left>div>div:first-child{
-  @apply font-bold text-5xl text-light-50 mb-4;
+    @apply font-bold text-5xl text-light-50 mb-4;
 }
 .left>div>div:last-child{
-@apply text-gray-200 text-sm;
+    @apply text-gray-200 text-sm;
 }
 .right .title{
-  @apply font-bold text-3xl text-gray-800 ;
+    @apply font-bold text-3xl text-gray-800;
 }
-
 .right>div{
-  @apply flex items-center justify-center my-5 space-x-2 text-gray-300;
+    @apply flex items-center justify-center my-5 text-gray-300 space-x-2;
 }
-
-.line{
-  @apply h-[1px] w-16 bg-gray-200;
+.right .line{
+    @apply h-[1px] w-16 bg-gray-200;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,30 @@ import WindiCSS from 'vite-plugin-windicss'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{   
+    port:3000, 
+    proxy:{
+      "/api": {
+        ws: true,
+        changeOrigin: true,
+        target: "http://ceshi13.dishait.cn",
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+    }
+  },
+  // devServer:{    
+  //   port: 3000,
+  //   proxy:{
+  //     "/api": {
+  //       ws: true,
+  //       changeOrigin: true,
+  //       target: "http://ceshi13.dishait.cn/",
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, "")
+  //     }
+  //   }
+  // },
   resolve:{
     alias:{
       "~":path.resolve(__dirname,"src")
